3178b7a43a44b540da698aa0729554d3ff309ac6
Nurettin Beser <nurettinbeser@yahoo.com>
2026-03-25 12:01:39 +0300

    user/admin.py is added. - use password to loging instead of username.

--- budgetproject/users/models.py
+++ budgetproject/users/models.py
@@ -5,3 +5,6 @@ import uuid
 class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
