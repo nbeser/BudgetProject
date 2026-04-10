6f9076b6c80815b3f843593e12b27c167120132c
Nurettin Beser <nurettinbeser@yahoo.com>
2026-04-10 15:11:16 +0300

    feat: -Language set change to Turkish since it is gonna launch in Turkey first. Later, languages will be updated. -Account's page created. You click account link on account nav, it takes you that account's detail page (which is yet to be updated). -nav active tags are created. For that purpose, style.css is updated

--- budgetproject/pages/urls.py
+++ budgetproject/pages/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.pages_index, name='pages_index'),
     path('dashboard/', views.user_dashboard, name='user_dashboard'),
     path('budgets/', views.get_budget, name='get_budget'),
+    path('accounts/<str:name>/', views.accounts, name='accounts_view'),
 ]
